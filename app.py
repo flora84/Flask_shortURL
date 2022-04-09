75a6cfa5580c048445ab648286d9517afbf834f8
floraChou <aaalll0222@gmail.com>
2022-04-10 05:23:58 +0800

    add file

---
+++ app.py
@@ -0,0 +1,41 @@
+"""
+This script runs the application using a development server.
+It contains the definition of routes and views for the application.
+"""
+from flask import Flask, render_template, request
+from flask.wrappers import Request
+import webbrowser
+import pyshorteners as psn
+
+app = Flask(__name__)
+
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app.wsgi_app
+
+Sort_to_Long_URL = {}
+count = 0
+
+@app.route('/')
+def root():
+    return render_template("index.html")
+@app.route('/sort_URL',methods=['GET', 'POST'])
+def sort_URL():
+    global Sort_to_Long_URL,count
+    if request.method=="POST":
+        longUrl =request.form['LongURL']
+        count+=1
+        Sort_to_Long_URL[count] = longUrl
+            
+    return render_template("index.html",longURL=longUrl,sortURL=("http://localhost:5555/go?sortURL="+str(count)))
+@app.route('/go',methods=['GET'])
+def go():
+    global Sort_to_Long_URL,count
+    if request.method=="GET":
+        key =request.args['sortURL']
+        print(Sort_to_Long_URL.get(key))
+
+    return render_template("index.html",URL=Sort_to_Long_URL[count]);
+if __name__ == '__main__':
+    import os
+    webbrowser. open('http://localhost:5555')
+    app.run('localhost',port='5555')
